f54a8a3221bd23f3d6c81640f76bb94f2d03f6d2
2AnnaLSaunders2 <anna@rsaunders.us>
2017-08-31 21:40:37 -0400

    Update script.js

--- build/script.js
+++ build/script.js
@@ -2,12 +2,12 @@
 
 showdown.setOption('customizedHeaderId', true);
 
-var liveURL = 'https://raw.githubusercontent.com/leigler/webadvanced-f17/master/contents/contents.md',
+var liveURL = 'https://raw.githubusercontent.com/2AnnaLSaunders2/2AnnaLSaunders2.github.io/master/contents/contents.md',
 	testURL = 'contents/contents.md'
 
 $.when($.ajax({
 	dataType: 'text',
-    url: testURL, 
+    url: liveURL, 
     complete: function(r){
 
     	/*
@@ -19,43 +19,13 @@ $.when($.ajax({
     	var converter = new showdown.Converter(),
 		text      = string,
 		html      = converter.makeHtml(text);
-
 		$( "body" ).append( $( html ) ); //introduce content
 
-		var h3amount = $("h3").length
-		$("h3").each(function(){
-
-			var h3index = $( "h3" ).index( this )		
-
-			if (!$(this).next().is("h3")) {
-				$(this).nextUntil("h3, h2, h1").wrapAll("<span class='h3-wrapper'></span>")
-			 	$(this).wrap("<div class='h3-prepend'></div>")
-				$(this).after("<span class='h3-closed'><div id='horizontal'></div><div id='vertical'></div></span>")
-				$(this).css({"display" : "inline"})
-
-			}
-
-			if($(this).parent(".h3-prepend").next().next().next().is("h3, h2, h1") || $(this).parent(".h3-prepend").next().next().is("h2, h1") || $(this).parent(".h3-prepend").next().is("h2, h1")){
-				$(this).parent(".h3-prepend").next(".h3-wrapper").css({"display" : "block"})
-				$(this).next(".h3-closed").remove()
-			}
-
-
-		})
     }
  })).then(function(){
 
 	$("body").css({"opacity" : 1, 
 					"transform" : "translateY(0px)"})
-
-
-	$(".h3-closed").click(function(){
-
-		$(this).parent(".h3-prepend").next(".h3-wrapper").css({"display" : "block"})
-		$(this).remove()
-
-	})
-
 
 })
 
